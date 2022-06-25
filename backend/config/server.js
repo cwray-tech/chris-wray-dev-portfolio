fa3235add5f7f579a61b5cf0534a6dff7bb9cc73
Christopher Wray <chris@solmediaco.com>
2022-06-25 10:54:12 -0700

    update database and server config for strapi v4

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b4e23acefac939b6cc132abebdd05830'),
+      keys: env.array("APP_KEYS"),
     },
   },
 });
